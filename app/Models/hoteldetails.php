f28c3a26ae657bcd298e711c173169f7961dd58a
Manshu777 <79352371+Manshu777@users.noreply.github.com>
2025-06-11 11:46:06 +0530

    wef42

--- app/Models/hoteldetails.php
+++ app/Models/hoteldetails.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
-class hoteldetails extends Model
+class Hoteldetails extends Model
 {
     use HasFactory;
 
